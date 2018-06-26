a9756290bd9028c474f84a203a7db61ce80badc9
Bonnie <blmattson@gmail.com>
2018-06-26 15:09:13 -0500

    move validation to point class

--- battleship.rb
+++ battleship.rb
@@ -6,38 +6,18 @@ class Ship
 
   def place
 
-    @point = Point.new
     #north = 0, east = 1, etc
-    @dir = rand(3)
+    @orientation = rand(3)
+    @point
 
-
-
-  end
-
-  def is_valid? (point, dir, length)
-
-    case dir
-      # north
-      # if the starting point on the y axis  plus the length of the ship is greater than 9, it is invalid
-      when 0
-         point.y + length > 9 ? false:true
-
-      # east
-      # if the starting point on the x axis  plus the length of the ship is greater than 9, it is invalid
-      when 1
-        point.x + length > 9 - length ? false:true
-
-      # south
-      # if the starting point on the y axis minus the ship length is less than 0 (the board size), it is invalid
-      when 2
-        point.y - length < 0 ? false:true
-
-      # west
-      # if the starting point on the y axis minus the ship length is less than 0 (the board size), it is invalid
-      when 3
-        point.y - length < 0 ? false:true
+    unless defined? @point && @point.is_valid?(@orientation, @length)
+      @point = Point.new
     end
+
+
   end
+
+
 end
 
 class Point
@@ -46,6 +26,32 @@ class Point
     @x = rand(9)
     @y = rand(9)
   end
+
+  def is_valid? (dir, length)
+
+    case dir
+      # north
+      # if the starting point on the y axis  plus the length of the ship is greater than 9, it is invalid
+      when 0
+        @y + length > 9 ? false:true
+
+      # east
+      # if the starting point on the x axis  plus the length of the ship is greater than 9, it is invalid
+      when 1
+        @x + length > 9 - length ? false:true
+
+      # south
+      # if the starting point on the y axis minus the ship length is less than 0 (the board size), it is invalid
+      when 2
+        @y - length < 0 ? false:true
+
+      # west
+      # if the starting point on the y axis minus the ship length is less than 0 (the board size), it is invalid
+      when 3
+        @x - length < 0 ? false:true
+    end
+  end
+
 end
 
 
