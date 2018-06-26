63aec004bf2d05d7a82c7b673a97cf59d87b28e8
Bonnie <blmattson@gmail.com>
2018-06-26 15:03:56 -0500

    first stab at validation

--- battleship.rb
+++ battleship.rb
@@ -13,6 +13,31 @@ class Ship
 
 
   end
+
+  def is_valid? (point, dir, length)
+
+    case dir
+      # north
+      # if the starting point on the y axis  plus the length of the ship is greater than 9, it is invalid
+      when 0
+         point.y + length > 9 ? false:true
+
+      # east
+      # if the starting point on the x axis  plus the length of the ship is greater than 9, it is invalid
+      when 1
+        point.x + length > 9 - length ? false:true
+
+      # south
+      # if the starting point on the y axis minus the ship length is less than 0 (the board size), it is invalid
+      when 2
+        point.y - length < 0 ? false:true
+
+      # west
+      # if the starting point on the y axis minus the ship length is less than 0 (the board size), it is invalid
+      when 3
+        point.y - length < 0 ? false:true
+    end
+  end
 end
 
 class Point
